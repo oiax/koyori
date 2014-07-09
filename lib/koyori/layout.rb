802679abcded2c07c2a6ad42cf8640d132967f5a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2014-07-09 14:48:03 +0900

    Embed title and css on layout.html

---
+++ lib/koyori/layout.rb
@@ -0,0 +1,19 @@
+module Koyori
+  class Layout
+    def initialize(body)
+      @body = body
+      @title = 'NO TITLE'
+    end
+
+    def output
+      basedir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+      path1 = File.expand_path(File.join(basedir, 'templates', 'layout.html'))
+      path2 = File.expand_path(File.join(basedir, 'templates', 'default.css'))
+      html = File.open(path1).read
+      css = File.open(path2).read.chomp
+      html.sub(/%TITLE%/, @title)
+        .sub(/%BODY%/, @body)
+        .sub(%r{</style>}, "\n#{css}\n</style>")
+    end
+  end
+end
