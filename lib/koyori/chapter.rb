24306e5fa0ea21c3b759a1f8415ce1e57c5c34e1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2014-07-09 22:57:15 +0900

    Format list

--- lib/koyori/chapter.rb
+++ lib/koyori/chapter.rb
@@ -5,45 +5,92 @@ module Koyori
     end
 
     def to_html
-      html = ''
-      line_number = 0
-      mode = 'normal'
-      path = nil
-      buffer = ''
+      @html = ''
+      @line_number = 0
+      @mode = 'normal'
+      @path = nil
+      @buffer = ''
       @source.gsub(/\r\n?/, "\n").each_line do |line|
-        line_number += 1
+        @line_number += 1
         line.chomp!
 
-        case line
-        when /\A(\#{1,6})\s+(.*)/
-          level = Regexp.last_match[1].length
-          text = Regexp.last_match[2]
-          html << Koyori::Heading.new(level, text).format
-        when /\A```(?:\[([^\]]+)\])?/
-          if mode == 'normal'
-            mode = 'pre'
-            buffer = ''
-            if Regexp.last_match[1]
-              path = Regexp.last_match[1]
-            end
-          else
-            html << Koyori::Pre.new(buffer, path).format
-            path = nil
-            mode = 'normal'
-          end
+        case @mode
+        when 'normal'
+          format_in_normal_mode(line)
+        when 'unordered_list'
+          format_in_unordered_list_mode(line)
+        when 'ordered_list'
+          format_in_ordered_list_mode(line)
+        when 'pre'
+          format_in_pre_mode(line)
         else
-          if mode == 'pre'
-            buffer << line + "\n"
-          else
-            if line.match(/\A\s*\z/)
-              html << "\n"
-            else
-              html << Koyori::Paragraph.new(line).format
-            end
-          end
+          raise
         end
       end
-      html
+
+      @html
+    end
+
+    private
+    def format_in_normal_mode(line)
+      case line
+      when /\A(\#{1,6})\s+(.*)/
+        level = Regexp.last_match[1].length
+        text = Regexp.last_match[2]
+        @html << Koyori::Heading.new(level, text).format
+      when /\A\*\s+(.*)/
+        @mode = 'unordered_list'
+        @buffer = line + "\n"
+      when /\A\d\.\s+(.*)/
+        @mode = 'ordered_list'
+        @buffer = line + "\n"
+      when /\A```(?:\[([^\]]+)\])?/
+        @mode = 'pre'
+        @buffer = ''
+        if Regexp.last_match[1]
+          @path = Regexp.last_match[1]
+        end
+      else
+        if line.match(/\A\s*\z/)
+          @html << "\n"
+        else
+          @html << Koyori::Paragraph.new(line).format
+        end
+      end
+    end
+
+    def format_in_unordered_list_mode(line)
+      case line
+      when /\A(\s\s)*\*\s+(.*)/
+        @buffer << line + "\n"
+      else
+        @html << Koyori::UnorderedList.new(@buffer).format
+        @mode = 'normal'
+        format_in_normal_mode(line)
+      end
+    end
+
+    def format_in_ordered_list_mode(line)
+      case line
+      when /\A(\s\s)*\d\.\s+(.*)/
+        @buffer << line + "\n"
+      else
+        @html << Koyori::OrderedList.new(@buffer).format
+        @mode = 'normal'
+        format_in_normal_mode(line)
+      end
+    end
+
+    def format_in_pre_mode(line)
+      case line
+      when /\A```\s*\z/
+        @html << Koyori::Pre.new(@buffer, @path).format
+        @buffer = ''
+        @path = nil
+        @mode = 'normal'
+      else
+        @buffer << line + "\n"
+      end
     end
   end
 end
--- lib/koyori/list.rb
+++ lib/koyori/list.rb
@@ -1,4 +1,58 @@
 module Koyori
   class List
+    def initialize(text)
+      @text = text
+    end
+
+    def format(tag, regexp)
+      buffer = "<#{tag}>\n"
+      @current_level = nil
+      @text.each_line do |line|
+        unless line.match(regexp)
+          raise "#{line}, #{tag}"
+        end
+        level = Regexp.last_match[1].length / 2
+        content = Regexp.last_match[2]
+        if @current_level == nil
+          buffer << "<li>#{CGI.escapeHTML(content)}"
+        elsif level == @current_level
+          buffer << "</li>\n"
+          buffer << "<li>#{CGI.escapeHTML(content)}"
+        elsif level == @current_level + 1
+          buffer << "\n<#{tag}>\n"
+          buffer << "<li>#{CGI.escapeHTML(content)}"
+        elsif level < @current_level
+          (@current_level - level).times do
+            buffer << "</li>\n"
+            buffer << "</#{tag}>\n"
+          end
+          buffer << "</li>\n"
+          buffer << "<li>#{CGI.escapeHTML(content)}"
+        end
+        @current_level = level
+      end
+
+      @current_level.times do
+        buffer << "</li>\n"
+        buffer << "</#{tag}>\n"
+        buffer << "</li>\n"
+      end
+
+      buffer << "</li>\n"
+      buffer << "</#{tag}>\n"
+      buffer
+    end
+  end
+
+  class UnorderedList < List
+    def format
+      super('ul', /\A(\s{2}*)\*\s+(.*)/)
+    end
+  end
+
+  class OrderedList < List
+    def format
+      super('ol', /\A(\s{2}*)\d\.\s+(.*)/)
+    end
   end
 end
